e3ea47c23a5335c47ae863482b7db4a00c5ddc39
Lalindya Rathnayake <lalindyacrathnayaka5951@gmail.com>
2026-05-06 14:14:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    if (this.getAttribute('href').startsWith("#")) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href'))
-        .scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-});
